7ae943cc4fc904544052cae799757658a0545d27
Mehmet Kutay Dündar <93616716+mehmetkutay07@users.noreply.github.com>
2024-02-16 06:31:01 +0300

    Update ABtest.py

--- ABtest.py
+++ ABtest.py
@@ -2,34 +2,6 @@
 # AB Testi ile BiddingYöntemlerinin Dönüşümünün Karşılaştırılması
 #####################################################
 
-#####################################################
-# İş Problemi
-#####################################################
-
-# Facebook kısa süre önce mevcut "maximumbidding" adı verilen teklif verme türüne alternatif
-# olarak yeni bir teklif türü olan "average bidding"’i tanıttı. Müşterilerimizden biri olan bombabomba.com,
-# bu yeni özelliği test etmeye karar verdi veaveragebidding'in maximumbidding'den daha fazla dönüşüm
-# getirip getirmediğini anlamak için bir A/B testi yapmak istiyor.A/B testi 1 aydır devam ediyor ve
-# bombabomba.com şimdi sizden bu A/B testinin sonuçlarını analiz etmenizi bekliyor.Bombabomba.com için
-# nihai başarı ölçütü Purchase'dır. Bu nedenle, istatistiksel testler için Purchasemetriğine odaklanılmalıdır.
-
-
-
-
-#####################################################
-# Veri Seti Hikayesi
-#####################################################
-
-# Bir firmanın web site bilgilerini içeren bu veri setinde kullanıcıların gördükleri ve tıkladıkları
-# reklam sayıları gibi bilgilerin yanı sıra buradan gelen kazanç bilgileri yer almaktadır.Kontrol ve Test
-# grubu olmak üzere iki ayrı veri seti vardır. Bu veri setleriab_testing.xlsxexcel’ininayrı sayfalarında yer
-# almaktadır. Kontrol grubuna Maximum Bidding, test grubuna AverageBiddinguygulanmıştır.
-
-# impression: Reklam görüntüleme sayısı
-# Click: Görüntülenen reklama tıklama sayısı
-# Purchase: Tıklanan reklamlar sonrası satın alınan ürün sayısı
-# Earning: Satın alınan ürünler sonrası elde edilen kazanç
-
 
 
 #####################################################
@@ -75,8 +47,8 @@ pd.set_option('display.max_rows', 10)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
 # Adım 1:  ab_testing_data.xlsx adlı kontrol ve test grubu verilerinden oluşan veri setini okutunuz. Kontrol ve test grubu verilerini ayrı değişkenlere atayınız.
-df_control = pd.read_excel("C:\\Users\\kdund\\PycharmProjects\\MiuulProject\\datasets\\ab_testing.xlsx", sheet_name="Control Group", engine="openpyxl")
-df_test = pd.read_excel("C:\\Users\\kdund\\PycharmProjects\\MiuulProject\\datasets\\ab_testing.xlsx", sheet_name="Test Group", engine="openpyxl")
+df_control = pd.read_excel("C:\\Users\\...\\ab_testing.xlsx", sheet_name="Control Group", engine="openpyxl")
+df_test = pd.read_excel("C:\\Users\\...\\ab_testing.xlsx", sheet_name="Test Group", engine="openpyxl")
 
 
 # Adım 2: Kontrol ve test grubu verilerini analiz ediniz.
